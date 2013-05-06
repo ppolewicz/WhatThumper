8ba81f8a182f0b65f9ebdb43590a251b4ca0f2c9
Paweł Polewicz <p.polewicz@gmail.com>
2013-05-06 22:23:05 +0200

    version 0.2, mostly contributed by Granite. Instead of console command it's now enough to look on the thumper for 2 seconds.

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ FireFall addon that lets You easily identify a thumper type
 
 Features:
 
-- if You issue /wt or /whatthumper command in chat and You are looking on a thumper, You will get the thumper name back.
+- if You hold Your recticle on a thumper for 2 seconds, You will get a system message with the thumper type
 
 FireFall forums thread: http://forums.firefallthegame.com/community/threads/addon-whatthumper-am-i-looking-at.145841/
--- WhatThumper/WhatThumper.lua
+++ WhatThumper/WhatThumper.lua
@@ -1,63 +1,23 @@
-require "string"
-require "lib/lib_Slash";
+require "lib/lib_Callback2"
 
-local pairs = pairs
-local math = math
-local string = string
+local cb_focus = Callback2.Create()
 
 PLUGIN_TAG = '[WhatThumper]'
 
--- event handlers
-
-function OnComponentLoad()
-	InitializeSlashCommands()
-end
-
--- Slash commands
-
-local SLASH_CMDS = {}
-
-function InitializeSlashCommands()
-	LIB_SLASH.BindCallback({slash_list = "whatthumper, wt", description = "", func = SLASH_CMDS.cmdroot})
-end
-
-SLASH_CMDS.cmdroot = function(text)
-	local option, message = text[1], text[2]
-	if option ~= nil then
-  		if not ( SLASH_CMDS[option] ) then log("["..option.."] Not Found") return nil end
-  		SLASH_CMDS[option](message)
-	else
-		ShowThumperType()
+function OnEntityFocus(args)
+	if args.entityId then
+		local info = Game.GetTargetInfo(args.entityId)
+		if info.type == "resourcenode" then
+			cb_focus:Bind(print_system_message, info.thumper_name)
+			cb_focus:Schedule(2)
+			return
+		end
+	end
+	if cb_focus:Pending() then
+		cb_focus:Cancel()
 	end
 end
-
-SLASH_CMDS.test = function(text)
-	log(tostring(targetInfo()))
-end
-
--- other functions
 
 function print_system_message(message)
 	Component.GenerateEvent("MY_SYSTEM_MESSAGE", {text=PLUGIN_TAG.." "..message})
-end
-
-function targetInfo()
-	local ri = Player.GetReticleInfo()
-	if not ri or not ri.entityId then
-		print_system_message("It seems that You are not looking on any entity")
-		return nil
-	end
-	local entityId = ri.entityId
-	return Game.GetTargetInfo(entityId)
-end
-
-function ShowThumperType()
-	local info = targetInfo()
-	if not info then
-		return
-	elseif not info.type == "resourcenode" or not info.thumper_name then
-		print_system_message("It seems that You are looking on something, but it's not a thumper")
-		return
-	end
-	print_system_message(info.thumper_name)
 end
